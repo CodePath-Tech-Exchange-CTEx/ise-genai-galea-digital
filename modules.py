6e9f55c3f4d9a0cc46e328bbd3d835cf86f61f41
amierd <amierldavis@gmail.com>
2026-02-19 03:41:12 +0000

    implement display_activity_summary in modules.py

--- modules.py
+++ modules.py
@@ -7,7 +7,10 @@
 # function other than the example.
 #############################################################################
 
+from datetime import datetime
 from internals import create_component
+import pandas as pd
+import streamlit as st
 
 
 # This one has been written for you as an example. You may change it as wanted.
@@ -35,8 +38,49 @@ def display_post(username, user_image, timestamp, content, post_image):
 
 
 def display_activity_summary(workouts_list):
-    """Write a good docstring here."""
-    pass
+    """Displays an 'activity summary' that showcases data about the user's
+    latest workout.
+
+    Args:
+        workouts_list: the list of the user's workouts 
+    """
+    st.header("Activity Summary")
+
+    if not workouts_list:
+        st.text("No workout history found.")
+        return
+
+    latest_workout = workouts_list[-1]
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    start = datetime.strptime(latest_workout['start_timestamp'], DATE_FORMAT)
+    end = datetime.strptime(latest_workout['end_timestamp'], DATE_FORMAT)
+
+    st.caption(start.strftime('%B %d, %Y'))
+
+    latest_workout_duration_mins = int((end - start).total_seconds() / 60)
+
+    # Metrics
+    cols = st.columns(4)
+    metrics = [
+        ("Duration", f"{latest_workout_duration_mins} mins"),
+        ("Total Steps", latest_workout.get('steps', 0)),
+        ("Distance", f"{latest_workout.get('distance', 0)} mi"),
+        ("Calories Burned", f"{latest_workout.get('calories_burned', 0)} cal")
+    ]
+
+    for col, (label, value) in zip(cols, metrics):
+        col.metric(label, value, border=True)
+
+    # Coordinate Map
+    start_coords = latest_workout['start_lat_lng']
+    end_coords = latest_workout['end_lat_lng']
+
+    map_data = pd.DataFrame([
+        {"lat": start_coords[0], "lon": start_coords[1], "name": "Start"},
+        {"lat": end_coords[0], "lon": end_coords[1], "name": "End"}
+    ])
+    st.map(map_data)
 
 
 def display_recent_workouts(workouts_list):
