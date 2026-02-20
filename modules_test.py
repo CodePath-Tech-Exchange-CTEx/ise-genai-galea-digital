290302737ef640c87ddc79889f872ec102025a1e
amierd <amierldavis@gmail.com>
2026-02-20 03:43:30 +0000

    remove unused import in modules_test.py

--- modules_test.py
+++ modules_test.py
@@ -6,7 +6,6 @@
 # You will write these tests in Unit 2.
 #############################################################################
 
-import json
 import unittest
 from streamlit.testing.v1 import AppTest
 from modules import display_post, display_activity_summary, display_genai_advice, display_recent_workouts
